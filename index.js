0c2ea570387a971065a747e0f44ed7993091f7d8
Ronaldo P. Lipata <lipata.ronaldo@gmail.com>
2021-03-13 11:52:41 +0800

    Conquering Responsive Layouts - Challenge

---
+++ index.js
@@ -0,0 +1,12 @@
+const toggleMenu = document.querySelector('.toggle-menu');
+let toggleBars = document.querySelector('.fa-bars');
+let toggleTimes = document.querySelector('.fa-times');
+let toggleDesktopMenu = document.querySelector('.desktop-menu');
+// const toggleMenu = document.querySelector('.toggle-menu');
+
+toggleMenu.addEventListener("click", function() {
+  toggleMenu.classList.toggle("active");
+  toggleBars.classList.toggle("hide");
+  toggleTimes.classList.toggle("active");
+  toggleDesktopMenu.classList.toggle("active");
+});
